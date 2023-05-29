871f40bb91634976b7417bd128e4590e60b1f7c9
not-Karot <52074617+not-Karot@users.noreply.github.com>
2023-05-29 16:07:05 +0200

    added gh load

--- flows/marvin.py
+++ flows/marvin.py
@@ -1,6 +1,6 @@
 from prefect import flow, get_run_logger
 from platform import node, platform
-
+from prefect.filesystems import GitHub
 
 @flow
 def marvin(user_input: str = "Marvin"):
@@ -10,4 +10,5 @@ def marvin(user_input: str = "Marvin"):
 
 
 if __name__ == "__main__":
+    github_block = GitHub.load("prefect-cloud-gcp")
     marvin()
